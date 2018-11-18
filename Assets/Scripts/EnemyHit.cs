c1955645f24a8b494f19867dad1a101eee1c309f
doctor25th <pragole@yahoo.com>
2018-11-17 22:29:19 -0800

    Trying to limit hits

--- Assets/Scripts/EnemyHit.cs
+++ Assets/Scripts/EnemyHit.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class EnemyHit : MonoBehaviour {
 
+	public float Timer;
+	public float Ready;
+
+
+void Update()
+{
+	Timer += Time.deltaTime;
+
+	//if (col.isTrigger != true )
+	//	{
+			
+	//		if (Timer >= 2){
+				//if (col.isTrigger != true )
+				//{
+					//PlayerStats You = col.gameObject.GetComponent<PlayerStats>();
+				//	You.curHealth -= 10;
+				//	Timer = 0;
+				//}
+				//else
+				//{
+				//Timer = 0;
+				//}
+		//	}
+	//	}
+}
 void OnTriggerEnter2D(Collider2D col)
 	{
 
-		if (col.isTrigger != true )
-		{
-			
 
-			PlayerStats You = col.gameObject.GetComponent<PlayerStats>();
-			You.curHealth -= 10;
-
-
-		}
 	}
 }
