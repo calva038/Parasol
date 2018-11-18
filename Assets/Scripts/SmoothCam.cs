0efe57529bea3fc60c9926974b75dc3f0429c7eb
Kevin Huang <khuan028@ucr.edu>
2018-11-17 20:10:13 -0800

    Smooth cam

---
+++ Assets/Scripts/SmoothCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class SmoothCam : MonoBehaviour {
+
+	public Transform target;
+	public Vector3 offset;
+	[Range(0.01f, 20f)]
+	public float sharpness = 1f;
+
+	void Start() {
+		Assert.IsNotNull(target, "Target must be assigned!");
+	}
+
+	void Update() {
+		float zpos = transform.position.z;
+		float t = Mathf.Clamp01(sharpness * Time.deltaTime);
+		Vector3 nextPos = Vector2.Lerp(transform.position, target.position, t);
+		nextPos.z = zpos;
+		transform.position = nextPos;
+	}
+}
