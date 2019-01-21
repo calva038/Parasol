229241aa0ab0a8448bc8bc7e1ac57102f22dc0e5
Christian D Alvarado <32400508+calva038@users.noreply.github.com>
2019-01-21 00:30:33 -0800

    Added Main Menu and Logo Video

---
+++ Assets/Scripts/SceneTimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTimer : MonoBehaviour
+{
+    void Start()
+    {
+
+        StartCoroutine(loadSceneAfterDelay(10));
+
+    }
+
+    IEnumerator loadSceneAfterDelay(float waitbySecs)
+    {
+
+        yield return new WaitForSeconds(waitbySecs);
+        Application.LoadLevel("Menu");
+    }
+}
