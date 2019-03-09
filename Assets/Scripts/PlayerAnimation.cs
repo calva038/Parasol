d4ad8d3aedbb8b91c955a641799506b93c223928
Kevin Huang <khuan028@ucr.edu>
2019-03-08 23:18:44 -0800

    Player now faces left/right depending on the key pressed. Removed CharacterAnimation.cs because obsolete.

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimation : CharacterAnimation {
+public class PlayerAnimation : MonoBehaviour {
 
 	//private bool skippableAnimation = true;
 	private bool repeat = false;
+	private float dirMultiplier = 1;	// Player initially faces right
 
 	[SerializeField] private Animator anim;
+	[SerializeField] protected CharacterPhysics2D characterPhysics;
 
-	protected override void Update () {
-		base.Update ();
+	private void Update () {
 		float xInput = Input.GetAxisRaw ("Horizontal");
 		anim.SetBool ("Ground", characterPhysics.IsGrounded);
 		anim.SetFloat ("Speed", Mathf.Abs (xInput)); //abs is used incase we move in the opposite direction so -1 = 1
@@ -19,6 +20,15 @@ public class PlayerAnimation : CharacterAnimation {
 			anim.SetTrigger ("Jump");
 		}
 
+		// Determine which way the player should face (left/right?)
+		Vector3 s = transform.localScale;
+		if(xInput > 0) {
+			dirMultiplier = 1;
+		}
+		else if(xInput < 0) {
+			dirMultiplier = -1;
+		}
+		transform.localScale = new Vector3( dirMultiplier * Mathf.Abs(s.x), s.y, s.z);
 	}
 
 	private void PokeRepeatCheck () {
